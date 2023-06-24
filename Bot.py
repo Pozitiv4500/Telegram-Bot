bda647a5435966d94c33a73d4cb31ff8d9f20358
Pozitiv4500 <123630113+Pozitiv4500@users.noreply.github.com>
2023-06-24 20:56:39 +0300

    Add files via upload

--- Bot.py
+++ Bot.py
@@ -1,13 +1,10 @@
 import logging
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher, types, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
+from aiogram.dispatcher.filters import Command, Text
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.dispatcher.filters import Command
-
-
-# Конфигурация логгирования
-logging.basicConfig(level=logging.INFO)
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeyboardMarkup, KeyboardButton
 
 # Инициализация бота и диспетчера
 bot = Bot(token='6231147103:AAGUuRf8mqQsiBPbHzHW_BgmB07lyyt24f4')
@@ -15,96 +12,118 @@ storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 
 
-# Определение состояний
-class Form(StatesGroup):
+# Класс, определяющий состояния бота
+class UserForm(StatesGroup):
     name = State()
     question = State()
+    contac = State()
+
+
+# Обработчик ввода текста после нажатия кнопки "Написать"
+@dp.message_handler(Text(equals="Написать"))
+async def ask_question(message: types.Message):
+    # Запуск диалога заново
+    await start_command(message)
 
 
 # Обработчик команды /start
-@dp.message_handler(Command('start'))
-async def cmd_start(message: types.Message):
-    await Form.name.set()
-    await message.reply("Давайте познакомимся. Как вас зовут?")
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    # Запуск диалога
+    await message.answer("Давайте познакомимся. Как вас зовут?")
+    await UserForm.name.set()
 
 
-# Обработчик ответа на вопрос "Как вас зовут?"
-@dp.message_handler(state=Form.name)
-async def process_name(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['name'] = message.text
-    
-    await Form.next()
-    await message.reply("Спасибо. Теперь опишите максимально подробно вашу задачу или вопрос:")
+@dp.callback_query_handler(text_contains="confirm")
+async def confirm_form(callback_query: types.CallbackQuery, state: FSMContext):
+    # Получение данных из состояния
+    user_data = await state.get_data()
 
-
-# Обработчик ответа на вопрос "Опишите вашу задачу или вопрос"
-@dp.message_handler(state=Form.question)
-async def process_question(message: types.Message, state: FSMContext):
-    async with state.proxy() as data:
-        data['question'] = message.text
-    
-    # Готовая форма для отправки
-    form_text = (
-        f"Проверьте, всё ли правильно?\n\n"
-        f"Имя: {data['name']}\n"
-        f"Вопрос: {data['question']}"
+    # Отправка сообщения в канал с данными формы
+    channel_message = (
+        f"Ник тг: {callback_query.from_user.username}\n"
+        f"Имя: {user_data['name']}\n"
+        f"Вопрос: {user_data['question']}\n"
+        f"Контакты: {user_data['contac']}"
     )
+    await bot.send_message(chat_id='-961129183', text=channel_message)
 
-    # Кнопки "Всё верно", "Редактировать" и "Остались вопросы? Написать"
+    # Отправка сообщения пользователю о успешной отправке формы
     keyboard = types.InlineKeyboardMarkup()
-    keyboard.add(
-        types.InlineKeyboardButton("Всё верно", callback_data="confirm"),
-        types.InlineKeyboardButton("Редактировать", callback_data="edit")
-    )
+    keyboard.add(types.InlineKeyboardButton("Остались вопросы? Написать", callback_data="ask_again"))
 
-    await message.reply(form_text, reply_markup=keyboard)
-
-
-# Обработчик нажатия на кнопку "Всё верно" или "Редактировать"
-@dp.callback_query_handler(lambda c: c.data in ["confirm", "edit", "ask_again"], state="*")
-async def process_callback(callback_query: types.CallbackQuery, state: FSMContext):
-    if callback_query.data == "confirm":
-        await callback_query.answer()
-        async with state.proxy() as data:
-            # Отправка сообщения в специальный канал
-            await bot.send_message(
-                -961129183,  # Замените на ID вашего канала
-                f"Ник тг: @{callback_query.from_user.username}\n"
-                f"Имя: {data['name']}\n"
-                f"Вопрос: {data['question']}"
-            )
-        
-
-        # Кнопка "Остались вопросы? Написать" в виде инлайновой кнопки
-        keyboard = types.InlineKeyboardMarkup()
-        keyboard.add(types.InlineKeyboardButton("Остались вопросы? Написать", callback_data="ask_again"))
-
-        await callback_query.message.answer(
+    await callback_query.message.answer(
             "Спасибо за обращение! Ваш вопрос в обработке, с вами свяжутся в ближайшее время. Обычно это около 24 часов.", reply_markup=keyboard
         )
 
-        await state.finish()
-    
-    elif callback_query.data == "edit":
-        await callback_query.answer()
-        await Form.name.set()
-        await callback_query.message.answer("Давайте познакомимся. Как вас зовут?")
-    
-    elif callback_query.data == "ask_again":
-        await Form.name.set()
-        await callback_query.message.answer("Давайте познакомимся. Как вас зовут?")
-
-
-# Обработчик команды /cancel
-@dp.message_handler(Command('cancel'), state="*")
-async def cmd_cancel(message: types.Message, state: FSMContext):
-    await message.answer("Вы отменили ввод.")
+    # Очистка состояния и переход к начальному состоянию
     await state.finish()
+
+    
+@dp.callback_query_handler(text_contains="edit")
+async def edit_form(callback_query: types.CallbackQuery, state: FSMContext):
+    # Очистка состояния и переход к начальному состоянию
+    await state.finish()
+
+    # Запуск диалога заново
+    await start_command(callback_query.message)
+
+@dp.callback_query_handler(text_contains="ask_again")
+async def ask_form(callback_query: types.CallbackQuery, state: FSMContext):
+    # Очистка состояния и переход к начальному состоянию
+    await state.finish()
+
+    # Запуск диалога заново
+    await start_command(callback_query.message)
+
+# Обработчик ввода имени
+@dp.message_handler(state=UserForm.name)
+async def process_name(message: types.Message, state: FSMContext):
+    # Сохранение имени в состоянии
+    await state.update_data(name=message.text)
+
+    # Запрос описания задачи или вопроса
+    await message.answer("Спасибо. Теперь опишите максимально подробно вашу задачу или вопрос:")
+    await UserForm.next()
+
+
+# Обработчик ввода задачи или вопроса
+@dp.message_handler(state=UserForm.question)
+async def process_question(message: types.Message, state: FSMContext):
+    # Сохранение задачи или вопроса в состоянии
+    await state.update_data(question=message.text)
+
+    # Запрос контактов
+    await message.answer("Теперь пожалуйста, поделитесь своими контактами:")
+    await UserForm.next()
+
+
+# Обработчик ввода контактов
+@dp.message_handler(state=UserForm.contac)
+async def process_contact(message: types.Message, state: FSMContext):
+    # Сохранение контактов в состоянии
+    await state.update_data(contac=message.text)
+
+    # Получение данных из состояния
+    user_data = await state.get_data()
+
+    # Формирование сообщения с готовой формой
+    form_message = f"Проверьте, всё ли правильно?\n\nИмя: {user_data['name']}\nВопрос: {user_data['question']}\nКонтакты: {user_data['contac']}"
+
+    # Формирование клавиатуры с кнопками
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    keyboard.add(
+        InlineKeyboardButton("Всё верно", callback_data="confirm"),
+        InlineKeyboardButton("Редактировать", callback_data="edit")
+    )
+
+    # Отправка сообщения с формой и клавиатурой
+    await message.answer(form_message, reply_markup=keyboard)
+
+    # Переход к состоянию ожидания подтверждения или редактирования
+    await UserForm.next()
 
 
 if __name__ == '__main__':
-    from aiogram import executor
-
     # Запуск бота
     executor.start_polling(dp, skip_updates=True)
